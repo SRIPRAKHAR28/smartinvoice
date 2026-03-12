8edb8735fce91cfa2987c9f6361a0e8d194025f7
SRIPRAKHAR28 <137150104+SRIPRAKHAR28@users.noreply.github.com>
2026-03-12 13:48:25 +0530

    Add Customers Page Component

---
+++ frontend/src/pages/Customers.js
@@ -0,0 +1,13 @@
+// Customers Page Component
+import React from 'react';
+
+function Customers() {
+  return (
+    <div className="customers-page">
+      <h2>Customers</h2>
+      <p>Manage your customers here</p>
+    </div>
+  );
+}
+
+export default Customers;
