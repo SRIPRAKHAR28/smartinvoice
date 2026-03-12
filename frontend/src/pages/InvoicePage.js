3244d2e94fe5965eda5b308d9e37c91f25d45c69
SRIPRAKHAR28 <137150104+SRIPRAKHAR28@users.noreply.github.com>
2026-03-12 13:50:46 +0530

    Add InvoicePage component

---
+++ frontend/src/pages/InvoicePage.js
@@ -0,0 +1,13 @@
+// Invoice Page Component
+import React from 'react';
+
+function InvoicePage() {
+  return (
+    <div className="invoice-page">
+      <h2>Invoices</h2>
+      <p>Manage your invoices here</p>
+    </div>
+  );
+}
+
+export default InvoicePage;
